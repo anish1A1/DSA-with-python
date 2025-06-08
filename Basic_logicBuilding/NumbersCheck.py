0958429cc5f08c5a4a0e561758b71c8fc0877cba
anish1A1 <alloozpat87@gmail.com>
2025-06-08 08:19:47 +0545

    To find the closest number

--- Basic_logicBuilding/NumbersCheck.py
+++ Basic_logicBuilding/NumbersCheck.py
@@ -46,9 +46,60 @@ def swap_two_number(a, b):
     # This can be done without creating new var.
     a,b = b,a
     print(a,b)
+    print("\n")
 
 
 if __name__ == "__main__":
     # print(sum_of_square_natural_number(8))
     swap_two_number(3,4)
 
+
+"""
+Find the number closest to n and divisible by m
+Last Updated : 13 Feb, 2025
+
+    Given two integers n and m (m != 0). Find the number closest to n and divisible by m. If there is more than one such number, then output the one having maximum absolute value.
+    
+    Examples: 
+
+    Input: n = 13, m = 4
+    Output: 12
+    Explanation: 12 is the closest to 13, divisible by 4.
+
+    Input: n = -15, m = 6
+    Output: -18
+    Explanation: Both -12 and -18 are closest to -15, but-18 has the maximum absolute value.
+    # maximum absolute value means greatest value
+"""
+
+# num closest to n    and   num divisible by m
+# if ouput more than 1 than choose highest
+
+def num_close_to_n_and_DivisibleBy_n(n : int,m : int) ->int:
+   
+   q:int = int(n/m)
+   #To find the closest num we first divide it and take a quotient
+   
+   # the 1st possible closest number
+   n1:int = m *q
+   
+#    To check the 2nd most closest number, we check th n and m ,multiple greater than 0 if yes than increase the quotient by 1 if not that decrease the queotient by -1
+ 
+   if((n*m )>0 ):
+       n2:int = (m * (q+1))
+   else: 
+       n2:int = (m* (q -1))   # if the m*n is in (-) than we knnow the second most closest has to be lower than n1 so we do (q-1)
+    
+    
+    #Now we need to return the closest value from n
+   if (abs(n - n1) < abs(n - n2)) :
+        return n1 
+        # Now when the value of n1 is less than n2 than the closest value is n1 
+    # for example there is 12, and 16 than n1 is the lowest that means n1 is the output so return it.
+   return n2   
+#else n2 is the closest
+   
+   
+   
+        
+print(num_close_to_n_and_DivisibleBy_n(-15,6))
