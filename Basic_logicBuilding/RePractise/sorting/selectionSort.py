fd86b61f4a87f9caf2e0893827c92fb0941d44fa
anish1A1 <alloozpat87@gmail.com>
2025-06-29 18:43:08 +0545

    completed relearning sorting

--- Basic_logicBuilding/RePractise/sorting/selectionSort.py
+++ Basic_logicBuilding/RePractise/sorting/selectionSort.py
@@ -119,3 +119,80 @@ def merge(left_side, right_side):
 my_list = [64, 25, 12, 22, 11]
 sorted_list = merge_sort(my_list)
 print(f"Sorted list: {sorted_list}")
+
+
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    
+    return merge(left, right)
+
+def merge(left , right):
+    
+    result = []
+    i = j =0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i +=1
+        else:
+            result.append(right[j])
+            j +=1 
+    
+    result.extend(left[i:])
+    result.extend(right[j:])
+    
+    return result
+
+# Example usage:
+my_list = [64, 25, 12, 22, 11]
+sorted_list = merge_sort(my_list)
+print(f"Sorted list: {sorted_list}")
+
+
+
+
+
+def quick_sort(arr, start, end):
+        
+    if start < end:
+        
+        middle_index = partition(arr, start, end)
+        quick_sort(arr, start, middle_index - 1)
+        quick_sort(arr, middle_index + 1, end)
+        
+
+def partition(arr, start , end):
+    
+    pivot = arr[end]
+    i =  start - 1
+    
+    for j in range(start, end):
+        if arr[j] <= pivot:
+            i+=1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[end] = arr[end], arr[i + 1]
+    
+    return i+1
+
+data2 = [1, 7, 4, 1, 10, 9, 2]
+print("Unsorted Array")
+print(data2)
+
+size = len(data2)
+
+quick_sort(data2, 0, size - 1)
+
+
+print('Sorted Array in Ascending Order:')
+print(data2)
+
+
