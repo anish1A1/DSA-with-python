405670481815bd3c76b3b6500472a9164d9473c4
anish1A1 <alloozpat87@gmail.com>
2025-06-29 19:58:13 +0545

    largest element in array

---
+++ Basic_logicBuilding/Arrays/LargestElement.py
@@ -0,0 +1,24 @@
+
+# To find the largest element in an array.
+
+
+def findLargest(arr):
+    arr.sort()
+    return arr[-1]
+
+# 0(nlog) and space 0(1)  since sorting and indexing
+
+
+
+def findLargestOptimal(arr):
+    largest = arr[0]   #ASsusme first index is largest
+    
+    for i in range(len(arr)):
+        if arr[i] > largest:
+            largest = arr[i]
+    return largest
+# 0(n)   and space 0(1)
+
+nums = [2,43,65,231,5]
+print(findLargest(nums))
+print(findLargestOptimal(nums))
