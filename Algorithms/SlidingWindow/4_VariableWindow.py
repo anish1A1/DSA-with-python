2cfa1bc06e3e3b5e0824f165cf801ef3f5b225af
anish1A1 <alloozpat87@gmail.com>
2025-12-14 08:51:18 +0545

    how to create a variable Window

---
+++ Algorithms/SlidingWindow/4_VariableWindow.py
@@ -0,0 +1,24 @@
+
+# Universal Variable Window Template
+
+"""
+left = 0
+for right in range(len(arr)):
+    # include arr[right] into window
+
+    while window is invalid:
+        # remove arr[left]
+        left += 1
+"""
+    # update answer
+
+"""
+Variable Window Pattern (CORE LOGIC)
+3 Steps:
+
+Expand window → move right
+
+Fix violation → move left
+
+Update answer
+"""
