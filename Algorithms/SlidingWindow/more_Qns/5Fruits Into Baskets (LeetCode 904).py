364db675d0a31c05acccbed3033b8165fd9e1bb1
anish1A1 <alloozpat87@gmail.com>
2025-12-17 21:34:55 +0545

    qns t o bbe done

---
+++ Algorithms/SlidingWindow/more_Qns/5Fruits Into Baskets (LeetCode 904).py
@@ -0,0 +1,16 @@
+"""
+5. Fruits Into Baskets (LeetCode 904)
+
+Difficulty: Medium
+
+Problem
+
+You are given an array fruits.
+You can pick at most two types of fruits.
+Return the maximum number of fruits you can pick.
+
+Example
+Input: fruits = [1,2,1]
+Output: 3
+Explanation: Pick fruits of type 1 and 2.
+"""
