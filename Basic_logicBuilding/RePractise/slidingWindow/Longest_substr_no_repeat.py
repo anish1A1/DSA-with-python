e58bd405c0fc894e593a289c17eeabff0a046b8e
anish1A1 <alloozpat87@gmail.com>
2026-01-16 10:06:24 +0545

    checked the difference

---
+++ Basic_logicBuilding/RePractise/slidingWindow/Longest_substr_no_repeat.py
@@ -0,0 +1,9 @@
+"""
+4. Longest Substring Without Repeating Characters
+- Problem: Given a string, find the length of the longest substring without repeating characters.
+
+- Example:
+- Input: s = "abcabcbb"
+- Output: 3 (substring "abc")
+
+"""
