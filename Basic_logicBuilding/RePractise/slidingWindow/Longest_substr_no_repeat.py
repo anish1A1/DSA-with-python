ebe384687b4725a4f2b922da02853dcaddddbf57
anish1A1 <alloozpat87@gmail.com>
2026-01-16 10:12:31 +0545

    Longest Substring Without Repeating Characters

--- Basic_logicBuilding/RePractise/slidingWindow/Longest_substr_no_repeat.py
+++ Basic_logicBuilding/RePractise/slidingWindow/Longest_substr_no_repeat.py
@@ -7,3 +7,19 @@
 - Output: 3 (substring "abc")
 
 """
+
+def non_repeating_Substr(s):
+    seen = set()
+    left =0
+    max_length = 0
+    window_length = 0
+    
+    for right in range(len(s)):
+        while s[right] in seen:
+            seen.remove(s[left])
+            left += 1
+        seen.add(s[right])
+        window_length = right - left + 1
+        max_length = max(max_length, window_length)
+    return max_length
+print(non_repeating_Substr(s = "abcabcbb"))
