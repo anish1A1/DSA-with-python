191b8893ed23254d339cbef464dd91ec16920677
Anish Jaiswal <alloozpat87@gmail.com>
2026-07-20 08:11:30 +0545

    program to find the product of all natural numbers

--- selfPrac/Arrays/reprac/simpleLogic/displayNumber.py
+++ selfPrac/Arrays/reprac/simpleLogic/displayNumber.py
@@ -10,3 +10,20 @@ def reverseOrder(nums):
 
 reverseOrder([1,2,3,4,5,6])
 
+#Write a program to find the product of all natural numbers from 1 to n (factorial of n).
+
+def productNatuNum(nums):
+    product = 1
+    for i in nums:
+        product *= i
+    print("The product of natural number is ",product)
+
+productNatuNum([1,2,3,4,5,6])
+
+def productNatuNum(nums):
+    product = 1
+    for i in range(len(nums)):
+        product *= i
+    print("The product of natural number is ",product)
+
+productNatuNum([1,2,3,4,5,6])
