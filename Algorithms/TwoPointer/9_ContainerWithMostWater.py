f810372bc31c3e1661a1fbda8f96f3bde94ed8e1
Anish Jaiswal <alloozpat87@gmail.com>
2026-08-12 06:31:12 +0545

    Optimal solution for Container with most water

--- Algorithms/TwoPointer/9_ContainerWithMostWater.py
+++ Algorithms/TwoPointer/9_ContainerWithMostWater.py
@@ -29,6 +29,28 @@ n == height.length
 
 from typing import List
 
+def maxArea(height:List[int]):
+    left = 0
+    right = len(height) - 1
+    area = 0
+    max_area = 0
+    
+    while left < right:
+        area = (right - left) * min(height[left], height[right])
+        # here width is right - left, the total distance between them
+        max_area = max(area, max_area)
+        
+        if height[left] < height[right]:
+            left += 1
+        elif height[right] < height[left]:
+            right -=1
+        else:  #if element are equal move any one pointer.
+            left += 1
+            
+    return max_area
+
+print(maxArea([1,8,6,2,5,4,8,3,7]))
+
 
 def maxAreaBrute(height: List[int]):  
     
@@ -37,9 +59,9 @@ def maxAreaBrute(height: List[int]):
     
     for i in range(len(height)):
         for j in range(i+1 ,len(height)):
-            area = (j - 1) * min(height[i], height[j])
-            # since j is the fast pointer it's position will be the width. 
-            # And also we added j by 1 to start brute force when reomved 1 we get its current array index.
+            area = (j - i) * min(height[i], height[j])
+            # j is the fast pointer. 
+            # To get the width(total distance of each elements in i and j)-> j-i.
             
             max_area = max(max_area, area)
             
