bc16432a555e8f7bc01211a3c80ab31c6ec720b1
Anish Jaiswal <alloozpat87@gmail.com>
2026-08-14 10:04:45 +0545

    Using decorator with args and kwargs

--- Python_Interview/2_Decorators.py
+++ Python_Interview/2_Decorators.py
@@ -43,5 +43,52 @@ def say_hello(name1, name2):
 retun_hello = say_hello('Sara', "Ansh")
 print(retun_hello)
 
+print('\n')
+# With *args and **kwargs in Decorator
 
-# With 
+# Creating a logger decorator with show what is running and and the function names being runned.
+
+import functools
+
+def logger_decorator(func):
+    
+    # Best practice: preserves function metadata
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        
+        print(f"Executing function ${func.__name__} with args: {args} and kwargs: {kwargs}")
+       
+        # Forward everything to the original function
+        result = func(*args, **kwargs)
+        print(f"${func.__name__} finished execution.")
+        
+        return result
+    return wrapper
+# Always try to use functools.wraps it keeps the metadata of the function so, that it will not return None when asked about what this function does.
+
+@logger_decorator
+def greet(name, message="Hello"):
+    return f"{message}, {name}"
+
+@logger_decorator
+def add_number(a,b,c,d):
+    return a+b+c+d
+
+@logger_decorator
+def multiple_number(*args):
+    result = 1  
+     # Initialize result to 1 because we are multiplying
+     
+    for i in args:
+        result *= i
+    return result
+    
+    
+    
+print(greet("Anish", "Nihau"))
+print("\n")
+print(add_number(1,3,4,56))
+print('\n')
+print(multiple_number(2,2,3,5,70,3,1,90,923))
+# *args catches extra positional arguments as a tuple
+# **kwargs catches extra keyword arguments as a dictionary
