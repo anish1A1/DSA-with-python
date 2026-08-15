371ffdbb75044ac48a7ec271ef90d02f668b9ad1
Anish Jaiswal <alloozpat87@gmail.com>
2026-08-15 07:00:26 +0545

    Fruits in the basket problem solved

--- Algorithms/SlidingWindow/more_Qns/5Fruits Into Baskets (LeetCode 904).py
+++ Algorithms/SlidingWindow/more_Qns/5Fruits Into Baskets (LeetCode 904).py
@@ -14,3 +14,27 @@ Input: fruits = [1,2,1]
 Output: 3
 Explanation: Pick fruits of type 1 and 2.
 """
+from collections import defaultdict
+
+def fruit_pick_2(arr):
+    seen = defaultdict(int)
+    left = 0
+    max_length = 0
+    
+    for right in range(len(arr)):
+        seen[arr[right]] += 1
+        
+        while len(seen) > 2:
+            seen[arr[left]] -= 1
+            if seen[arr[left]] == 0:
+                del seen[arr[left]]
+            left += 1
+        max_length = max(max_length, right - left + 1)
+    return max_length
+
+print(fruit_pick_2([1,2,1]))
+print(fruit_pick_2([1,2,1,2,1]))
+print(fruit_pick_2([1,2,1,3,3,1]))
+
+
+        
